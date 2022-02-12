c6d770e65ee5096ba91f8df9c8ea96e360c6559d
Aleksei Anisimov <suxelshock@gmail.com>
2022-02-12 23:18:32 +0300

    Unused files are removed

--- tmp/kotlin/ru.alira.pets.login.ui/ImageVO.kt
+++
@@ -1,9 +0,0 @@
-package ru.alira.pets.login.ui
-
-import dev.icerock.moko.resources.ImageResource
-
-sealed class ImageVO {
-    data class Resource(
-        val res: ImageResource
-    ):ImageVO()
-}
