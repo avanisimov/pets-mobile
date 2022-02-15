40deea365192afff7ad364dc0d761b24640c0de9
Aleksei Anisimov <suxelshock@gmail.com>
2022-02-15 22:53:26 +0300

    FeedScreen in iOS improvements

--- iosApp/petsIOS/FeedScreen.swift
+++ iosApp/petsIOS/FeedScreen.swift
@@ -19,34 +19,55 @@ struct FeedScreen: View {
                 let items:[ViewObject] = itemsArray.toArray()
                 List(items, id: \.id) { item in
                     if let feedItem = item as? FeedItemVO {
-                        VStack {
-                            HStack {
-                                if let imageUrl = feedItem.author.image as? ImageVO.Url {
-                                    AsyncImage(url: URL(string: imageUrl.url)) { image in
-                                        image
-                                                 .resizable()
-                                                 .scaledToFill()
-                                    } placeholder: {
-                                        ProgressView()
-                                    }
-                                        .frame(width: 48, height: 48)
-                                        .background(Color.gray)
-                                        .clipShape(Circle())
-                                }
-                                VStack {
-                                    Text(String(feedItem.author.name.localized()))
-                                    Text(String(feedItem.date.localized()))
-                                        
-                                }
-                            }
-                            Text(String(feedItem.text.localized()))
-                                .lineLimit(5)
-                        }
+                        FeedItemWidget(feedItem: feedItem)
+                            .listRowSeparator(.hidden)
                     }
                 }
+                .listStyle(.plain)
             }
         }
     }
 }
 
-
+struct FeedItemWidget:View {
+    var feedItem: FeedItemVO
+    var body: some View {
+        VStack {
+            HStack {
+                if let imageUrl = feedItem.author.image as? ImageVO.Url {
+                    AsyncImage(url: URL(string: imageUrl.url)) { image in
+                        image
+                                 .resizable()
+                                 .scaledToFill()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                        .frame(width: 48, height: 48)
+                        .background(Color.gray)
+                        .clipShape(Circle())
+                }
+                VStack {
+                    Text(String(feedItem.author.name.localized()))
+                    Text(String(feedItem.date.localized()))
+                        
+                }
+            }
+            .frame(
+                minWidth: 0,
+                maxWidth: .infinity,
+                minHeight: 0,
+                maxHeight: .infinity,
+                alignment: .topLeading
+            )
+            Text(String(feedItem.text.localized()))
+                .lineLimit(4)
+                .frame(
+                    minWidth: 0,
+                    maxWidth: .infinity,
+                    minHeight: 0,
+                    maxHeight: .infinity,
+                    alignment: .topLeading
+                )
+        }
+    }
+}
